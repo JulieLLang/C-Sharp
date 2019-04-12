53dc8b810868869a47a0c25db119e31eb5a3b1dc
Julie Lang <j_lang1@icloud.com>
2019-04-11 18:51:38 -0700

    New Commit

---
+++ Step113/Step113/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step113
+{
+    public class Math
+    {
+         
+        public static void AddTwoNumbers(int numOne, int numTwo)
+        {
+            Console.WriteLine("Your results for addition: " + numOne + " + 100" + " = " + (numOne + 100));
+        }
+    }
+}
