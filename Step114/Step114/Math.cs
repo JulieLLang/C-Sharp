51a84d6efe9d87fc076874070466598562a7c5d1
Julie Lang <j_lang1@icloud.com>
2019-04-11 19:49:27 -0700

    New Commit

---
+++ Step114/Step114/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step114
+{
+    public class Math
+    {
+        public static int AddTwoNumbers(int numOne)
+        {
+            return 100 + numOne;
+        }
+
+        public static double AddTwoNumbers(double numTwo)
+        {
+            return numTwo * 10.3;
+        }
+        public static string AddTwoNumbers(string numThree)
+        {
+            return numThree;
+        }
+    }
+}
---
+++ Step114/Step114/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step114
+{
+    class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            Console.WriteLine("Input a whole number: ");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(+ Math.AddTwoNumbers(numOne));
+            Console.ReadLine();
+
+            Console.WriteLine("Input a number with a decimal: ");
+            double numTwo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.AddTwoNumbers(numTwo));
+            Console.ReadLine();
+
+            Console.WriteLine("Input another whole number: ");
+            int numThree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.AddTwoNumbers(numThree * 10));
+            Console.ReadLine();
+
+        }
+
+    }
+}
