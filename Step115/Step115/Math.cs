7e8e66eaf1c6b5f5068bff5725ec5e87946539b9
Julie Lang <j_lang1@icloud.com>
2019-04-11 22:18:28 -0700

    New Commit

---
+++ Step115/Step115/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step115
+{
+    public class Math
+    {
+
+
+        public static int AddTwoNumbers(int numOne, int numTwo = 0)
+
+        {
+            return numOne * 2 + numTwo;
+        }
+    }
+}
+
---
+++ Step115/Step115/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Math math = new Math();
+            //Math.AddTwoNumbers(numOne, numTwo = 0);
+
+            Console.WriteLine("Please input a number: ");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadLine();
+
+            Console.WriteLine("Please input another number, only if you want to: ");
+            string inputTwoStr = Console.ReadLine();
+
+            Console.ReadLine();
+
+
+
+            if (!string.IsNullOrEmpty(inputTwoStr))
+            {
+                int inputTwo = Convert.ToInt32(inputTwoStr);
+
+                Console.WriteLine("Thanks for your inputs! (" + inputOne + " * 2) " + "+ " + inputTwo + " = " + Math.AddTwoNumbers(inputOne, inputTwo));
+
+            }
+            else
+            {
+                Console.WriteLine("Without a second number your problem works out to: " + inputOne + " * 2 " + " = " + Math.AddTwoNumbers(inputOne));
+            }
+            Console.ReadLine();
+        }
+    }
+}
