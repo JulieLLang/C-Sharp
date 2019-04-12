734c24a4ee61b34555eea13c219ba39f3895fec6
Julie Lang <j_lang1@icloud.com>
2019-04-11 17:43:24 -0700

    New Commit

---
+++ Step111/Step111/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step111
+{
+    public class Math
+    {
+        public static int AddTwoNumbers(int numOne)
+        {
+            return 100 + numOne;
+        }
+
+        public static int DivideTwoNumbers(int numOne)
+        {
+            return numOne / 2;
+        }
+        public static int MultiplyTwoNumbers(int numOne)
+        {
+            return 5 * numOne;
+        }
+    }
+}
---
+++ Step111/Step111/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number for math to occur:");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your results for addition: " + numOne +" + 100 " + " = " + Math.AddTwoNumbers(numOne)); 
+            Console.ReadLine();
+
+            Console.WriteLine("Your results for division: " + numOne + " / 2 " + " = " + Math.DivideTwoNumbers(numOne));
+            Console.ReadLine();
+
+            Console.WriteLine("Your results for multiplication: " + numOne + " * 5 " + " = " + Math.MultiplyTwoNumbers(numOne));
+            Console.ReadLine();
+
+        }
+    }
+}
