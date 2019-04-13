b6c448b4cd037017a41f2b0a4b162a5d0ab52696
Julie Lang <j_lang1@icloud.com>
2019-04-13 01:22:37 -0700

    New Commit

---
+++ Step131/Step131/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step131
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+    }
+}
+//1. Make the Employee class take a generic type parameter.
+//2. Add a property to the Employee class called "things" and 
+//have its data type be a generic list matching the generic type of the class.
---
+++ Step131/Step131/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Instantiate an Employee object with type "string" as its generic parameter.
+            //Assign a list of strings as the property value of Things.
+            Employee<string> Employees = new Employee<string>() { Things = new List<string> { "Rebecca", "Alice", "Steve" } };
+
+            //3. Instantiate an Employee object with type "string" as its generic parameter.
+            //Assign a list of strings as the property value of Things.
+            Employee<int> EmployeeID = new Employee<int>() { Things = new List<int> { 101, 572, 1033 } };
+
+            //5. Create a loop that prints all of the Things to the Console.
+            foreach (string Employee in Employees.Things)
+            {
+                Console.WriteLine("Name: " + Employee);
+            }
+
+            foreach (int ID in EmployeeID.Things)
+            {
+                Console.WriteLine("Employee ID: " + ID);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
