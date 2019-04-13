d50cc1a6f01bb94c45231b2743a3ff983d44b572
Julie Lang <j_lang1@icloud.com>
2019-04-13 02:47:35 -0700

    New Commit

---
+++ Step137/Step137/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step137
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount = 1091.59M };
+
+            Console.WriteLine("Amount = ${0}", number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
