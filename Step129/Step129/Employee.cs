df19cc43f70f885412845f23e5450990b2d04b0d
Julie Lang <j_lang1@icloud.com>
2019-04-12 20:39:05 -0700

    New Commit

---
+++ Step129/Step129/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step129
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        public bool isActive { get; set; }
+            
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            return Equals(emp1.Id, emp2.Id);
+        }
+        public static bool operator!= (Employee emp1, Employee emp2)
+        {
+            return !Equals(emp1.Id, emp2.Id);
+        }
+    }
+}
---
+++ Step129/Step129/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1, isActive = true };
+            Employee emp2 = new Employee() { FirstName = "Sample", LastName = "Student", Id = 2, isActive = true };
+            Employee emp3 = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1, isActive = true };
+
+
+            Console.WriteLine(emp1 == emp2);
+            Console.WriteLine(emp2 == emp3);
+            Console.WriteLine(emp1 == emp3);
+
+            Console.ReadLine();
+        }
+    }
+}
