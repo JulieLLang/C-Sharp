edc7bce5576469938be16efaf59f09a354a293eb
Julie Lang <j_lang1@icloud.com>
2019-04-12 17:31:43 -0700

    New Commit

---
+++ Step124/Step124/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
+//5. Inside the Main() method, instantiate an Employee 
+//object with firstName "Sample" and lastName "Student". 
+//Call the SayName() method on the object.
