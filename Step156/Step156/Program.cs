b620370ffedcd3539d4bd00fc4c73fef4d16522f
Julie Lang <j_lang1@icloud.com>
2019-04-14 21:00:22 -0700

    New Commit

---
+++ Step156/Step156/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateValue = new DateTime();
+            dateValue = DateTime.Now;
+
+            Console.WriteLine("{0}", dateValue);
+            Console.WriteLine("Please enter a number:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("In {0} hours, the date and time will be: \n{1}.", x, dateValue.AddHours(x));
+
+            Console.ReadLine();
+        }
+    }
+}
