e93ec0e13677ff64043ebc435a26c4985e9c29a4
Julie Lang <j_lang1@icloud.com>
2019-04-14 20:33:08 -0700

    New Commit

---
+++ Step154/Step154/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Step154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Documents\GitHub\C-Sharp\Step154\Logs\log.txt", true))
+            {
+                file.WriteLine(yourNumber);
+            }
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Student\Documents\GitHub\C-Sharp\Step154\Logs\log.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+                Console.ReadLine();
+        }
+    }
+}
+
+        
