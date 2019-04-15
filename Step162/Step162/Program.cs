2159f32deb89df5667b27acd9ba3637ff6954c30
Julie Lang <j_lang1@icloud.com>
2019-04-15 14:34:19 -0700

    New Commit

---
+++ Step162/Step162/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string companyName = "JAR";
+
+            Console.WriteLine("Welcome to {0}. \nHow many JAR's of Jam would you like?", companyName);
+            var jam = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("That will be: " + jam + " JAR's of Jam on the way!\nWe guarantee you won't be disappointed!Thank you for your business.");
+            Console.ReadLine();
+        }
+    }
+}
+
