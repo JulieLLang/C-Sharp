e20f7610246b043da9503d45d7a2eef611136454
Julie Lang <j_lang1@icloud.com>
2019-04-16 00:34:02 -0700

    New Iteration

---
+++ Step162/Step162/Constructor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step162
+{
+    public class Employee
+    {
+        //CONST VAR CREATION
+        const string name = "Squirrel Head Enterprise";
+
+        public string First;
+        public string Last;
+        public int Id;
+
+        //CHAIN 2 CONSTRUCTORS TOGETHER
+        public Employee(string txt1) : this(txt1, "-", 500)
+        {
+            var First = txt1; //VAR CREATION USING "VAR"
+        }
+
+        public Employee(string txt1, string txt2) : this(txt1, txt2, 0)
+        {
+            First = txt1;
+            Last = txt2;
+        }
+
+        public Employee(string txt1, string txt2, int integer)
+        {
+            First = txt1;
+            Last = txt2;
+            Id = integer;
+        }
+
+        public void printName()
+        {
+            Console.WriteLine(First + " " + Last + " " + "Employee Id: " + Id + "\nIs the CEO of" + " " + name);
+        }
+    }
+}
--- Step162/Step162/Program.cs
+++ Step162/Step162/Program.cs
@@ -8,16 +8,12 @@ namespace Step162
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            const string companyName = "JAR";
+         static void Main(string[] args)
+            {
+            Employee ex = new Employee("Clarence Cavins");
+                    ex.printName();
 
-            Console.WriteLine("Welcome to {0}. \nHow many JAR's of Jam would you like?", companyName);
-            var jam = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("That will be: " + jam + " JAR's of Jam on the way!\nWe guarantee you won't be disappointed!Thank you for your business.");
-            Console.ReadLine();
+                Console.ReadLine();
+            }
         }
     }
-}
-
